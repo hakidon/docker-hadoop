37d7c2c2c6aab4ae9854e5e127007d60d395f2bf
hakidon <mohdanihakim@gmail.com>
2023-12-29 16:28:17 +0800

    Update apply manifest

--- k8s/slave/apply-manifest.sh
+++ k8s/slave/apply-manifest.sh
@@ -1,16 +1,10 @@
 #!/bin/bash
 
 # Apply the Kubernetes manifests
-sudo microk8s kubectl apply -f namenode-service.yaml
 sudo microk8s kubectl apply -f datanode-deployment.yaml
 sudo microk8s kubectl apply -f hadoop-env-configmap.yaml
 sudo microk8s kubectl apply -f hadoop-datanode-persistentvolumeclaim.yaml
-sudo microk8s kubectl apply -f historyserver-deployment.yaml
-sudo microk8s kubectl apply -f hadoop-historyserver-persistentvolumeclaim.yaml
-sudo microk8s kubectl apply -f namenode-deployment.yaml
-sudo microk8s kubectl apply -f hadoop-namenode-persistentvolumeclaim.yaml
 sudo microk8s kubectl apply -f nodemanager1-deployment.yaml
-sudo microk8s kubectl apply -f resourcemanager-deployment.yaml
 
 # Add more apply commands if needed
 

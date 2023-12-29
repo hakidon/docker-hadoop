8ae46ee93c16f66e49a74b909b756e71b34108d3
hakidon <mohdanihakim@gmail.com>
2023-12-29 23:01:50 +0800

    Update latest

--- k8s/master/apply-manifest.sh
+++ k8s/master/apply-manifest.sh
@@ -4,14 +4,11 @@
 sudo microk8s kubectl apply -f namenode-service.yaml
 sudo microk8s kubectl apply -f resourcemanager-service.yaml
 sudo microk8s kubectl apply -f historyserver-service.yaml
-sudo microk8s kubectl apply -f datanode-deployment.yaml
 sudo microk8s kubectl apply -f hadoop-env-configmap.yaml
-sudo microk8s kubectl apply -f hadoop-datanode-persistentvolumeclaim.yaml
 sudo microk8s kubectl apply -f historyserver-deployment.yaml
 sudo microk8s kubectl apply -f hadoop-historyserver-persistentvolumeclaim.yaml
 sudo microk8s kubectl apply -f namenode-deployment.yaml
 sudo microk8s kubectl apply -f hadoop-namenode-persistentvolumeclaim.yaml
-sudo microk8s kubectl apply -f nodemanager1-deployment.yaml
 sudo microk8s kubectl apply -f resourcemanager-deployment.yaml
 
 # Add more apply commands if needed
--- k8s/slave/apply-manifest.sh
+++ k8s/slave/apply-manifest.sh
@@ -2,6 +2,7 @@
 
 # Apply the Kubernetes manifests
 sudo microk8s kubectl apply -f datanode-deployment.yaml
+sudo microk8s kubectl apply -f datanode-service.yaml
 sudo microk8s kubectl apply -f hadoop-env-configmap.yaml
 sudo microk8s kubectl apply -f hadoop-datanode-persistentvolumeclaim.yaml
 sudo microk8s kubectl apply -f nodemanager1-deployment.yaml

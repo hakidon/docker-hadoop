0e88d91b2a7b869cd49c55ffca47c5b26f8be762
hakidon <mohdanihakim@gmail.com>
2024-01-02 23:27:15 +0800

    Update permission

--- k8s/script/enable-permissions.sh
+++ k8s/script/enable-permissions.sh
@@ -5,6 +5,7 @@ chmod +x ./init-mk8s.sh
 chmod +x ./enable-mk8s.sh
 chmod +x ../slave/apply-manifest.sh
 chmod +x ../slave/update-hosts.sh
+chmod +x ../slave/update-nodemanager.sh
 chmod +x ../slave/delete-all.sh
 chmod +x ../master/apply-manifest.sh
 chmod +x ../master/update-hosts.sh

2209ccf1e960044bd12fe5afd0f1600d0dc897c5
hakidon <mohdanihakim@gmail.com>
2024-01-02 15:47:39 +0800

    Update yet again

--- k8s/script/enable-permissions.sh
+++ k8s/script/enable-permissions.sh
@@ -3,10 +3,10 @@
 # Set executable permissions for file xxx
 chmod +x ./init-mk8s.sh
 chmod +x ./enable-mk8s.sh
-chmod +x ../slave/apply-manifests.sh
+chmod +x ../slave/apply-manifest.sh
 chmod +x ../slave/update-hosts.sh
 chmod +x ../slave/delete-all.sh
-chmod +x ../master/apply-manifests.sh
+chmod +x ../master/apply-manifest.sh
 chmod +x ../master/update-hosts.sh
 chmod +x ../master/delete-all.sh
 

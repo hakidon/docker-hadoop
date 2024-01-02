d10dd6dcf60d4148ca0932900b8e28d83a145f85
hakidon <mohdanihakim@gmail.com>
2024-01-02 15:46:01 +0800

    Update permission

--- k8s/script/enable-permissions.sh
+++ k8s/script/enable-permissions.sh
@@ -3,11 +3,11 @@
 # Set executable permissions for file xxx
 chmod +x ./init-mk8s.sh
 chmod +x ./enable-mk8s.sh
-chmod +x ./slave/apply-manifests.sh
-chmod +x ./slave/update-hosts.sh
-chmod +x ./slave/delete-all.sh
-chmod +x ./master/apply-manifests.sh
-chmod +x ./master/update-hosts.sh
-chmod +x ./master/delete-all.sh
+chmod +x ../slave/apply-manifests.sh
+chmod +x ../slave/update-hosts.sh
+chmod +x ../slave/delete-all.sh
+chmod +x ../master/apply-manifests.sh
+chmod +x ../master/update-hosts.sh
+chmod +x ../master/delete-all.sh
 
 echo "Executable permissions for all execubtables."

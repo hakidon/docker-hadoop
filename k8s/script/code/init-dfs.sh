56c0c9c97e15f929fe8690f8b29309c25ffc04e0
hakidon <mohdanihakim@gmail.com>
2024-01-02 16:40:03 +0800

    Update init dfs

--- k8s/script/code/init-dfs.sh
+++ k8s/script/code/init-dfs.sh
@@ -2,7 +2,7 @@
 
 hdfs dfs -mkdir -p /user/root
 hdfs dfs -mkdir input
-hdfs dfs -put /home/input.txt input-500/input-500.txt
-hdfs dfs -put /home/input.txt input-1000/input-1000.txt
-hdfs dfs -put /home/input.txt input-5000/input-5000.txt
-hdfs dfs -put /home/input.txt input-10000/input-10000.txt
+hdfs dfs -put /home/input-500.txt input-500/input-500.txt
+hdfs dfs -put /home/input-1000.txt input-1000/input-1000.txt
+hdfs dfs -put /home/input-5000.txt input-5000/input-5000.txt
+hdfs dfs -put /home/input-10000.txt input-10000/input-10000.txt

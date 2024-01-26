14e189aba4f0cb37290c175a8a0415fd37a0b78b
hakidon <mohdanihakim@gmail.com>
2024-01-27 00:15:26 +0800

    real_deploy

--- deployment/master/enable-permissions.sh
+++ deployment/master/enable-permissions.sh
@@ -3,7 +3,7 @@
 # Set executable permissions for file xxx
 chmod +x ./init-mk8s.sh
 chmod +x ./enable-mk8s.sh
-chmod +x .apply-manifest.sh
+chmod +x ./apply-manifest.sh
 chmod +x ./delete-all.sh
 chmod +x ./update-hosts.sh
 
